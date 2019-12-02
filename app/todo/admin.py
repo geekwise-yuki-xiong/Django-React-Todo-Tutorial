a2eb664ab86a02e035cabdfbea42fc0ba461d8d0
geekwise-yuki-xiong <geekwise.yuki.xiong@gmail.com>
2019-12-02 18:56:53 +0000

    Working progress up to and including Todo DRF portion

--- app/todo/admin.py
+++ app/todo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from todo.models import Todo
 
 # Register your models here.
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'completed')
+
+admin.site.register(
+    Todo,
+    TodoAdmin,
+)
--- app/todo/views.py
+++ app/todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from todo.serializers import TodoSerializer
+from todo.models import Todo
 
 # Create your views here.
+
+class TodoViewSet(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
